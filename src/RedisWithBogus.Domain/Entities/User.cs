7f84334403802e2154743460e7906dc20cb64263
Jamil Abrantes <jamilabrantes.dev@outlook.com>
2020-10-06 14:37:36 -0300

    Update User.cs

--- src/RedisWithBogus.Domain/Entities/User.cs
+++ src/RedisWithBogus.Domain/Entities/User.cs
@@ -7,15 +7,5 @@ namespace RedisWithBogus.Domain.Entities
         public Guid Id { get; set; }
         public string Name { get; set; }
         public DateTime Birthday { get; set; }
-
-        /*
-        https://medium.com/@renato.groffe/asp-net-core-2-0-implementando-cache-em-apis-rest-cd2df219f13b
-        https://medium.com/@nelson.souza/net-core-fake-data-442fed37718d
-        https://medium.com/@nelson.souza/cache-distribu%C3%ADdo-com-net-core-e-redis-f19e361cfd0b
-        https://medium.com/@nelson.souza/net-core-user-secrets-1934a62a7ab6
-        https://medium.com/@renato.groffe/asp-net-core-dicas-%C3%BAteis-para-o-dia-a-dia-de-um-desenvolvedor-parte-3-9e5ddc1283b0
-        https://stackoverflow.com/questions/37276932/what-the-difference-between-fromroute-and-frombody-in-a-web-api
-        https://github.com/JamilEAbrantes/V8NET/tree/master/src/V8Net.Domain/UsuarioBaseContext
-         */
     }
 }
